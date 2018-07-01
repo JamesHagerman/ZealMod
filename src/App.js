57ac30d2e77f0cd1692e8efe15a162f118016828
James Hagerman <james.hagerman@gmail.com>
2018-07-01 03:16:19 -0700

    Adding preliminary usb stuff. Adding a homepage key to package.json so I can deploy this to github pages.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,36 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 //import './App.css';
 
+// Known vendor IDs for ZealPC keyboards:
+const knownDevices = []
+// Configure which USB interface and endpoint to use:
+let keyboardInterface = 1
+let keyboardEndpoint = 1
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.allowUSBDevice() = this.allowUSBDevice.bind(this)
+  }
+
+
+
+  allowUSBDevice() {
+    // Build a filter to only select some of the currently connected USB devices:
+    const filters = knownDevices.map(vendorId => ({ vendorId }))
+
+    // Tell the browser to ask the User for access to the available devices:
+    navigator.usb.requestDevice({ filters })
+      .then(device => {
+        // Note: Device Objects returned from the WebUSB APIs do not play nice with JSON.stringify()
+        console.log(`Newly allowed USB device:`, device)
+        //this.updateDeviceList()
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,6 +41,7 @@ class App extends Component {
         <p>
           The goal of this project is to use WebUSB to send new keymaps to the keyboard via raw HID messages.
         </p>
+        <button onClick={this.allowUSBDevice}>Connect to keyboard</button>
       </div>
     );
   }
